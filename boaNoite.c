ac1874962abd97c498a0fd1107b208c523a428eb
Joao Victor Mira de Oliveria <54165704+JoaoVictorMiraOliveira@users.noreply.github.com>
2019-08-21 20:30:08 -0300

    Add files via upload

---
+++ boaNoite.c
@@ -0,0 +1,23 @@
+//Palavras reservadas em letra minuscula
+
+//Abrindo as bibliotecas basicas 1º passo. Todas as bibliotecas tem (.h)
+#include <stdio.h>
+
+//Função ou Metodo a Ser executada Primeiro (main) Função Principal
+void main()
+{
+	printf("Boa Noite"); //(printf) Escreve na Tela do Usuário
+	printf("\nBoa Tarde"); //(\n) Serve para pular ou quebrar a linha
+	printf("\nBom Dia"); 
+	
+	printf("\n\tBom Dia"); //(printf) Escreve na Tela do Usuário
+	printf("\n\tBoa Tarde"); //(\n) Serve para pular ou quebrar a linha
+	printf("\n\tBoa Noite"); //(\t) Serve para dar um Tab 
+	
+	printf("\n\"Joao Victor\" "); //(\" \") Serve para escrever "" (aspas) na Tela
+	printf("\nJoao Victor \\ Vitor Prado"); //(\\) Serve para escrever \ (barra) na Tela
+
+	printf("\nEscrevendo e Voltando o Cursor\rOi"); //(\r) Serve para voltar para a 1º Coluna
+	
+}
+
