b57b4aa5dd445c460cd7d2fc2a5607d9b46e2644
Joao Victor Mira de Oliveria <54165704+JoaoVictorMiraOliveira@users.noreply.github.com>
2019-08-21 20:00:34 -0300

    Add files via upload

---
+++ boaNoite.c
@@ -0,0 +1,13 @@
+//Palavras reservadas em letra minuscula
+
+//Abrindo as bibliotecas basicas 1º passo. Todas as bibliotecas tem (.h)
+#include <stdio.h>
+
+//Função ou Metodo a Ser executada Primeiro (main) Função Principal
+void main()
+{
+	printf("Boa Noite"); //Escrevendo na Tela do Usuário
+	printf("Boa Tarde"); 
+	printf("Bom Dia");
+}
+
